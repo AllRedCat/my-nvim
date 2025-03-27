9e877441f6d0bde11b4f5de26e43f0d9dda4b5ec
Gabriel Red <42196149+AllRedCat@users.noreply.github.com>
2025-03-27 20:41:42 -0300

    New config

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,21 +1,52 @@
 return {
   -- add gruvbox
   -- { "ellisonleao/gruvbox.nvim" },
-  -- { "navarasu/onedark.nvim" },
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "navarasu/onedark.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        float = "transparent",
+      },
+    },
+  },
+  -- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   -- { "elvessousa/sobrio.vim" },
-  { "rebelot/kanagawa.nvim" },
+  -- { "rebelot/kanagawa.nvim", transparent = true },
   -- { "nobbmaestro/nvim-andromeda" },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      -- colorscheme = "onedark",
+      colorscheme = "onedark",
       -- colorscheme = "sobrio",
       -- colorscheme = "catppuccin",
-      colorscheme = "kanagawa",
+      -- colorscheme = "kanagawa",
       -- colorscheme = "andromeda",
     },
   },
 }
+
+-- return {
+--   -- add gruvbox
+--   {
+--     "ellison/gruvbox.nvim",
+--     opt = {
+--       transparent = true,
+--       styles = {
+--         sidebars = "transparent",
+--         float = "transparent",
+--       },
+--     },
+--   },
+
+--   -- Configure LazyVim to load gruvbox
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "gruvbox",
+--     },
+--   },
+-- }
